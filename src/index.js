acd04388af914c11a2cbee873ea4eb3036caa42a
Łukasz <lsliwaradioluz@gmail.com>
2021-09-10 12:45:41 +0200

    Promise.all project and category logic

--- src/index.js
+++ src/index.js
@@ -5,9 +5,11 @@ import createClient from './client';
 export default async function CommercetoolsConfiguratorModule (moduleOptions) {
   const client = createClient(this);
   /**
-   * We await the generatei18nConfiguration call so that all of the values
+   * We await the method calls so that all of the values
    * find their way to the config before the VSFContext gets created
    */
-  await generatei18nConfiguration(client, moduleOptions, this);
-  fetchCommercetoolsCategories(client, moduleOptions, this)
+  Promise.all([
+    await generatei18nConfiguration(client, moduleOptions, this),
+    await fetchCommercetoolsCategories(client, moduleOptions, this)
+  ])
 }
